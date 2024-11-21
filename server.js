364345e983081a176b26a413269b47382e7486cf
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-11-21 13:48:46 -0300

    :package: config server.js

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
 import express from "express";
 
 const posts = [
-    {
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        descricao: "Paisagem deslumbrante",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        descricao: "Cachorro fofo",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        descricao: "Citação inspiradora",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        descricao: "Comida deliciosa",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        descricao: "Viagem incrível",
-        imagem: "https://placecats.com/millie/300/150"
-    }
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
 ];
 
+
 const app = express();
-app.arguments(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send();
-})
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
